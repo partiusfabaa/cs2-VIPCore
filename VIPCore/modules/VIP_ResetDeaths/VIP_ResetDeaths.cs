5213965567bdb56bf68c8c7c85b6e7a2b733256f
hyo <96542489+partiusfabaa@users.noreply.github.com>
2024-01-29 12:57:24 +0500

    1

--- VIPCore/modules/VIP_ResetDeaths/VIP_ResetDeaths.cs
+++ VIPCore/modules/VIP_ResetDeaths/VIP_ResetDeaths.cs
@@ -3,56 +3,66 @@ using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
 using Modularity;
 using VipCoreApi;
+using static VipCoreApi.IVipCoreApi;
 
 namespace VIP_ResetDeaths;
 
-public class VipArmor : BasePlugin, IModulePlugin
+public class VipResetDeaths : BasePlugin, IModulePlugin
 {
     public override string ModuleAuthor => "WodiX";
     public override string ModuleName => "[VIP] ResetDeaths";
     public override string ModuleVersion => "v1.0.0";
 
+    private ResetDeaths _resetDeaths;
     private IVipCoreApi _api = null!;
     private static readonly string Feature = "ResetDeaths";
 
     public void LoadModule(IApiProvider provider)
     {
         _api = provider.Get<IVipCoreApi>();
-        _api.RegisterFeature(Feature, IVipCoreApi.FeatureType.Hide);
+        _resetDeaths = new ResetDeaths(this, _api);
+        _api.RegisterFeature(_resetDeaths, FeatureType.Hide);
     }
-
-    [ConsoleCommand("rd", "ResetDeaths")]
-    public void OnResetDeathsCommand(CCSPlayerController player, CommandInfo command)
-    {
-        if (!_api.IsClientVip(player) || !_api.PlayerHasFeature(player, Feature))
-        {
-            _api.PrintToChat(player, _api.GetTranslatedText("vip.NoAccess"));
-            return;
-        }
-        if (_api.GetPlayerFeatureState(player, Feature) is IVipCoreApi.FeatureState.NoAccess) return;
-
-        if (player.ActionTrackingServices == null) return;
-
-        var scoreDeaths = player.ActionTrackingServices!.MatchStats;
-
-        var playerPawnValue = player.PlayerPawn.Value;
-
-        
-
-        var scoreValue = _api.GetFeatureValue<bool>(player, Feature);
-
-        if (scoreValue)
-        {
-            if (playerPawnValue != null)
-            {
-                scoreDeaths.Deaths = 0;
-                _api.PrintToChat(player, _api.GetTranslatedText("vip.ResetDeaths"));
-            }
-        }
-    }
-
+    
     public override void Unload(bool hotReload)
     {
-        _api.UnRegisterFeature(Feature);
+        _api.UnRegisterFeature(_resetDeaths);
+    }
+}
+
+public class ResetDeaths : VipFeatureBase
+{
+    public override string Feature { get; }
+
+    public ResetDeaths(VipResetDeaths vipResetDeaths, IVipCoreApi api) : base(api)
+    {
+        vipResetDeaths.AddCommand("css_rd", "ResetDeaths", OnResetDeathsCommand);
+        vipResetDeaths.AddCommand("css_resetdeaths", "ResetDeaths", OnResetDeathsCommand);
+        vipResetDeaths.AddCommand("rd", "ResetDeaths", OnResetDeathsCommand);
+    }
+
+    private void OnResetDeathsCommand(CCSPlayerController? player, CommandInfo commandinfo)
+    {
+        if (player == null) return;
+        
+        if (!IsClientVip(player) || !PlayerHasFeature(player))
+        {
+            PrintToChat(player, GetTranslatedText("vip.NoAccess"));
+            return;
+        }
+
+        if (GetPlayerFeatureState(player) is FeatureState.NoAccess) return;
+
+        if (player.ActionTrackingServices == null) return;
+        
+        var scoreDeaths = player.ActionTrackingServices!.MatchStats;
+        var playerPawnValue = player.PlayerPawn.Value;
+        var scoreValue = GetFeatureValue<bool>(player);
+
+        if (!scoreValue) return;
+        if (playerPawnValue == null) return;
+        
+        scoreDeaths.Deaths = 0;
+        PrintToChat(player, GetTranslatedText("vip.ResetDeaths"));
     }
 }
