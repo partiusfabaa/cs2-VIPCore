c618cad3a09a66f9c4f10c4b9ec2fdf62dfb45d4
pandathebeasty <vladpaiu99@gmail.com>
2024-04-30 09:11:28 +0300

    Update Defuser & Zeus modules

--- VIPCore/modules/VIP_Defuser/VIP_Defuser.cs
+++ VIPCore/modules/VIP_Defuser/VIP_Defuser.cs
@@ -1,4 +1,5 @@
-﻿using CounterStrikeSharp.API.Core;
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Capabilities;
 using VipCoreApi;
 
@@ -8,7 +9,7 @@ public class VipDefuser : BasePlugin
 {
     public override string ModuleAuthor => "panda";
     public override string ModuleName => "[VIP] Defuser";
-    public override string ModuleVersion => "v1.0";
+    public override string ModuleVersion => "v1.1";
     
     private Defuser? _defuser;
     private IVipCoreApi? _api;
@@ -38,14 +39,32 @@ public class Defuser : VipFeatureBase
     public Defuser(IVipCoreApi api) : base(api)
     {
     }
+    
+    public static bool HasWeapon(CCSPlayerController player, string weaponName)
+    {
+        if (!player.IsValid || !player.PawnIsAlive)
+            return false;
 
+        var pawn = player.PlayerPawn.Value;
+        if (pawn == null || pawn.WeaponServices == null)
+            return false;
+
+        foreach (var weapon in pawn.WeaponServices.MyWeapons)
+        {
+            if (weapon?.Value?.IsValid == true && weapon.Value.DesignerName?.Contains(weaponName) == true)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     public override void OnPlayerSpawn(CCSPlayerController player)
     {
         if (!PlayerHasFeature(player)) return;
         if (GetPlayerFeatureState(player) is IVipCoreApi.FeatureState.Disabled
             or IVipCoreApi.FeatureState.NoAccess) return;
         
-        if(player.TeamNum == 3)
+        if (player.TeamNum == 3 && !HasWeapon(player, "item_defuser"))
         {
             player.GiveNamedItem("item_defuser");
         }
