32a4fd68c91fd7307bbe3a76a35e43eb1a6be7f2
luxury fabka <96542489+partiusfabaa@users.noreply.github.com>
2024-05-19 10:39:09 +0900

    Update VIPCore.cs

--- VIPCore/VIPCore/VIPCore.cs
+++ VIPCore/VIPCore/VIPCore.cs
@@ -19,7 +19,7 @@ public class VipCore : BasePlugin
 {
     public override string ModuleAuthor => "thesamefabius";
     public override string ModuleName => "[VIP] Core";
-    public override string ModuleVersion => "v1.2.9";
+    public override string ModuleVersion => "v1.3.0";
 
     public Config Config { get; set; } = null!;
     public CoreConfig CoreConfig { get; set; } = null!;
