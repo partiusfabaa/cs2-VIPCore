7894cd18f800468acd44046a05777e141b76e5d7
Dawid Bepierszcz <41084667+daffyyyy@users.noreply.github.com>
2024-07-17 12:17:12 +0200

    Update VIP_Flags.cs
    Fix for CS2-SimpleAdmin and other plugins - not remove other flags than vip flags

--- VIPCore/modules/VIP_Flags/VIP_Flags.cs
+++ VIPCore/modules/VIP_Flags/VIP_Flags.cs
@@ -1,132 +1,117 @@
-﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Capabilities;
 using CounterStrikeSharp.API.Modules.Admin;
 using CounterStrikeSharp.API.Modules.Entities;
-using CounterStrikeSharp.API.Modules.Timers;
-using CounterStrikeSharp.API.Modules.Utils;
 using VipCoreApi;
 using static VipCoreApi.IVipCoreApi;
-using Timer = CounterStrikeSharp.API.Modules.Timers.Timer;
 
 namespace VIP_Flags;
 
 public class VipFlags : BasePlugin
 {
-    public override string ModuleAuthor => "thesamefabius";
-    public override string ModuleName => "[VIP] Flags";
-    public override string ModuleVersion => "v1.0.0";
-    
-    private IVipCoreApi? _api;
-    private Flags _flags;
+	public override string ModuleAuthor => "thesamefabius";
+	public override string ModuleName => "[VIP] Flags";
+	public override string ModuleVersion => "v1.0.0";
 
-    private PluginCapability<IVipCoreApi> PluginCapability { get; } = new("vipcore:core");
+	private IVipCoreApi? _api;
+	private Flags _flags;
 
-    public override void OnAllPluginsLoaded(bool hotReload)
-    {
-        _api = PluginCapability.Get();
+	private PluginCapability<IVipCoreApi> PluginCapability { get; } = new("vipcore:core");
 
-        if (_api == null) return;
+	public override void OnAllPluginsLoaded(bool hotReload)
+	{
+		_api = PluginCapability.Get();
 
-        _flags = new Flags(this, _api);
-        _api.RegisterFeature(_flags, FeatureType.Hide);
-    }
-    
-    public override void Unload(bool hotReload)
-    {
-        _api?.UnRegisterFeature(_flags);
-    }
+		if (_api == null) return;
+
+		_flags = new Flags(this, _api);
+		_api.RegisterFeature(_flags, FeatureType.Hide);
+	}
+
+	public override void Unload(bool hotReload)
+	{
+		_api?.UnRegisterFeature(_flags);
+	}
 }
 
 public class Flags : VipFeatureBase
 {
-    private readonly VipFlags _vipFlags;
-    public override string Feature => "flags";
-    private readonly Dictionary<ulong, List<string>> _flags = new();
-    private bool[] _clientDisconnected = new bool[70];
-    
-    public Flags(VipFlags vipFlags, IVipCoreApi api) : base(api)
-    {
-        _vipFlags = vipFlags;
-        vipFlags.RegisterEventHandler<EventPlayerDisconnect>((@event, _) =>
-        {
-            var player = @event.Userid;
+	private readonly VipFlags _vipFlags;
+	public override string Feature => "flags";
+	private readonly Dictionary<ulong, List<string>> _flags = new();
+	private bool[] _clientDisconnected = new bool[70];
 
-            if (player is null || !player.IsValid) return HookResult.Continue;
-            
-            if (IsClientVip(player) && PlayerHasFeature(player))
-            {
-                _clientDisconnected[player.Slot] = true;
-                RemovePlayerPermissions(player);
-            }
-            
-            return HookResult.Continue;
-        });
-        
-        vipFlags.AddCommand("css_testflag", "", (player, info) =>
-        {
-            if (player is null) return;
-            
-            player.PrintToChat($"{AdminManager.PlayerHasPermissions(player, "@css/viptestflag")}");
-        });
-    }
+	public Flags(VipFlags vipFlags, IVipCoreApi api) : base(api)
+	{
+		_vipFlags = vipFlags;
+		vipFlags.RegisterEventHandler<EventPlayerDisconnect>((@event, _) =>
+		{
+			var player = @event.Userid;
 
-    public override void OnPlayerLoaded(CCSPlayerController player, string group)
-    {
-        if (!PlayerHasFeature(player)) return;
-        
-        Timer timer = null!;
+			if (player is null || !player.IsValid || player.IsBot || _clientDisconnected[player.Slot]) return HookResult.Continue;
 
-        timer = _vipFlags.AddTimer(1f, () =>
-        {
-            if (_clientDisconnected[player.Slot])
-            {
-                timer.Kill();
-                return;
-            }
+			if (PlayerHasFeature(player))
+			{
+				_clientDisconnected[player.Slot] = true;
+				RemovePlayerPermissions(player);
+			}
 
-            if (player.Connected != PlayerConnectedState.PlayerConnected)
-                return;
+			return HookResult.Continue;
+		});
+	}
 
-            if (!_flags.ContainsKey(player.SteamID))
-                _flags.Add(player.SteamID, []);
-        
-            var flagsOrGroups = GetFeatureValue<List<string>>(player);
+	public override void OnPlayerLoaded(CCSPlayerController player, string group)
+	{
+		_vipFlags.AddTimer(1.5f, () =>
+		{
+			if (player == null || !player.IsValid || player.Connected != PlayerConnectedState.PlayerConnected) return;
 
-            var steamId = new SteamID(player.SteamID);
-            foreach (var flagOrGroup in flagsOrGroups)
-            {
-                if (flagOrGroup.StartsWith('@'))
-                    AdminManager.AddPlayerPermissions(steamId, flagOrGroup);
-                else
-                    AdminManager.AddPlayerToGroup(steamId, flagOrGroup);
-            
-                _flags[player.SteamID].Add(flagOrGroup);
-            }
-            
-            timer.Kill();
-        }, TimerFlags.REPEAT);
-    }
-    
-    public override void OnPlayerRemoved(CCSPlayerController player, string group)
-    {
-        RemovePlayerPermissions(player);
-    }
+			if (!IsClientVip(player) || !PlayerHasFeature(player))
+				return;
 
-    private void RemovePlayerPermissions(CCSPlayerController player)
-    {
-        if (!_flags.TryGetValue(player.SteamID, out var value)) return;
-        
-        var playerFlagsOrGroups = new List<string>(value);
+			if (!_flags.ContainsKey(player.SteamID))
+				_flags.Add(player.SteamID, []);
 
-        var steamId = new SteamID(player.SteamID);
-        foreach (var flagOrGroups in playerFlagsOrGroups)
-        {
-            if (flagOrGroups.StartsWith('@'))
-                AdminManager.RemovePlayerPermissions(steamId, flagOrGroups);
-            else
-                AdminManager.RemovePlayerFromGroup(steamId, groups: flagOrGroups);
-            value.Remove(flagOrGroups);
-        }
-    }
+			var flagsOrGroups = GetFeatureValue<List<string>>(player);
+			if (flagsOrGroups == null || flagsOrGroups.Count == 0) return;
+
+			var steamId = new SteamID(player.SteamID);
+			foreach (var flagOrGroup in flagsOrGroups)
+			{
+				if (flagOrGroup.StartsWith('@'))
+					AdminManager.AddPlayerPermissions(steamId, flagOrGroup);
+				else
+					AdminManager.AddPlayerToGroup(steamId, flagOrGroup);
+
+				_flags[player.SteamID].Add(flagOrGroup);
+			}
+
+			_clientDisconnected[player.Slot] = false;
+		});
+	}
+
+	/*
+	public override void OnPlayerRemoved(CCSPlayerController player, string group)
+	{
+		RemovePlayerPermissions(player);
+	}
+	*/
+
+	private void RemovePlayerPermissions(CCSPlayerController player)
+	{
+		if (!_flags.TryGetValue(player.SteamID, out var value)) return;
+
+		var playerFlagsOrGroups = new List<string>(value);
+
+		var steamId = new SteamID(player.SteamID);
+		foreach (var flagOrGroups in playerFlagsOrGroups)
+		{
+			if (flagOrGroups.StartsWith('@'))
+				AdminManager.RemovePlayerPermissions(steamId, flagOrGroups);
+			else
+				AdminManager.RemovePlayerFromGroup(steamId, groups: flagOrGroups);
+			value.Remove(flagOrGroups);
+		}
+	}
 }
 
