e97c7f760906dfe7c0cffcba3f3a4b221806ef1b
luxury fabka <96542489+partiusfabaa@users.noreply.github.com>
2024-08-15 19:38:19 +0900

    fix VIP_Flags module

--- VIPCore/modules/VIP_Flags/VIP_Flags.cs
+++ VIPCore/modules/VIP_Flags/VIP_Flags.cs
@@ -18,7 +18,7 @@ public class VipFlags : BasePlugin
     public override string ModuleAuthor => "thesamefabius";
     public override string ModuleName => "[VIP] Flags";
     public override string ModuleVersion => "v1.0.0";
-    
+
     private IVipCoreApi? _api;
     private Flags _flags;
 
@@ -33,7 +33,7 @@ public class VipFlags : BasePlugin
         _flags = new Flags(this, _api);
         _api.RegisterFeature(_flags, FeatureType.Hide);
     }
-    
+
     public override void Unload(bool hotReload)
     {
         _api?.UnRegisterFeature(_flags);
@@ -45,8 +45,7 @@ public class Flags : VipFeatureBase
     private readonly VipFlags _vipFlags;
     public override string Feature => "flags";
     private readonly Dictionary<ulong, List<string>> _flags = new();
-    private readonly bool[] _clientDisconnected = new bool[70];
-    
+
     public Flags(VipFlags vipFlags, IVipCoreApi api) : base(api)
     {
         _vipFlags = vipFlags;
@@ -55,13 +54,12 @@ public class Flags : VipFeatureBase
             var player = @event.Userid;
 
             if (player is null || !player.IsValid) return HookResult.Continue;
-            
+
             if (IsClientVip(player) && PlayerHasFeature(player))
             {
-                _clientDisconnected[player.Slot] = true;
                 RemovePlayerPermissions(player);
             }
-            
+
             return HookResult.Continue;
         });
     }
@@ -69,23 +67,17 @@ public class Flags : VipFeatureBase
     public override void OnPlayerLoaded(CCSPlayerController player, string group)
     {
         if (!PlayerHasFeature(player)) return;
-        
+
         Timer timer = null!;
 
         timer = _vipFlags.AddTimer(1f, () =>
         {
-            if (_clientDisconnected[player.Slot])
-            {
-                timer.Kill();
-                return;
-            }
-
             if (player.Connected != PlayerConnectedState.PlayerConnected)
                 return;
 
             if (!_flags.ContainsKey(player.SteamID))
                 _flags.Add(player.SteamID, []);
-        
+
             var flagsOrGroups = GetFeatureValue<List<string>>(player);
 
             var steamId = new SteamID(player.SteamID);
@@ -95,14 +87,14 @@ public class Flags : VipFeatureBase
                     AdminManager.AddPlayerPermissions(steamId, flagOrGroup);
                 else
                     AdminManager.AddPlayerToGroup(steamId, flagOrGroup);
-            
+
                 _flags[player.SteamID].Add(flagOrGroup);
             }
-            
+
             timer.Kill();
         }, TimerFlags.REPEAT);
     }
-    
+
     public override void OnPlayerRemoved(CCSPlayerController player, string group)
     {
         RemovePlayerPermissions(player);
@@ -111,11 +103,9 @@ public class Flags : VipFeatureBase
     private void RemovePlayerPermissions(CCSPlayerController player)
     {
         if (!_flags.TryGetValue(player.SteamID, out var value)) return;
-        
-        var playerFlagsOrGroups = new List<string>(value);
 
         var steamId = new SteamID(player.SteamID);
-        foreach (var flagOrGroups in playerFlagsOrGroups)
+        foreach (var flagOrGroups in value.ToList())
         {
             if (flagOrGroups.StartsWith('@'))
                 AdminManager.RemovePlayerPermissions(steamId, flagOrGroups);
@@ -125,4 +115,3 @@ public class Flags : VipFeatureBase
         }
     }
 }
-
