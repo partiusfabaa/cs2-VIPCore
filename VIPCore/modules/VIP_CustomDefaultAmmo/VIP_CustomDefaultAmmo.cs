12439ebe7f04590689768d878c360db6bbb07b54
pandathebeasty <vladpaiu99@gmail.com>
2024-08-30 09:57:54 +0300

    Update VIP_CustomDefaultAmmo.cs

--- VIPCore/modules/VIP_CustomDefaultAmmo/VIP_CustomDefaultAmmo.cs
+++ VIPCore/modules/VIP_CustomDefaultAmmo/VIP_CustomDefaultAmmo.cs
@@ -1,4 +1,4 @@
-﻿﻿﻿using CounterStrikeSharp.API;
+﻿﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Capabilities;
 using CounterStrikeSharp.API.Core.Attributes;
